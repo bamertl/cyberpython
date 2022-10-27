4c211c4dc1fe26d62aa2690b905abde4f4b24150
lukas <bamert@protonmail.com>
2022-10-27 17:42:49 +0200

    gwag

--- Ex6.py
+++ Ex6.py
@@ -5,6 +5,8 @@ import numpy as np
 from decimal import *
 from tqdm import tqdm
 from Cyphers import k
+import string
+import textwrap
 
 c2 = int(c2, 16)
 ex = 65537
@@ -35,12 +37,22 @@ def F(R, K):
 
 
 def decrypt(cypher):
-    cypher_length = len(cypher)
-    print(cypher_length.bit_length() / 10)
+    print(len(cypher))
+    cypher = cypher[63:-1]
+    blocks = textwrap.wrap(cypher, 16)
     keys = [pow(k, x, 2 ** 64) for x in range(1, 17)]
     # reverse the keys
-    liminus1 = cypher[: len(cypher) / 2]
-    riminus1 = cypher[len(cypher) / 2:]
+    print(len(cypher))
+    for block in blocks:
+        result = decrypt_block(block, keys)
+
+
+def decrypt_block(block, keys):
+    half = int(len(block) / 2)
+    liminus1 = block[: half]
+    riminus1 = block[half:]
+    liminus1 = int(liminus1, 16)
+    riminus1 = int(riminus1, 16)
     li = ""
     ri = ""
     for i in keys:
@@ -49,7 +61,7 @@ def decrypt(cypher):
         liminus1 = li
         riminus1 = riminus1
     result = li + ri
-    print(result)
+    return result
 
 
 if __name__ == "__main__":
